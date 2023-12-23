22cfdc7eb1caedab84f2799577c05a52473e6355
Rinor Kastrati <rinor.kastrati@pabau.com>
2023-12-23 21:08:28 +0100

    chore(client): add package for hexgrid

---
+++ hex-client/app/gameroom/interface.ts
@@ -0,0 +1,11 @@
+export const Pieces = {
+	none: 0,
+	king: 1,
+	pawn: 2,
+	knight: 3,
+	bishop: 4,
+	rook: 5,
+	queen: 6,
+	white: 8,
+	black: 16,
+  }
--- hex-client/app/gameroom/page.tsx
+++ hex-client/app/gameroom/page.tsx
@@ -1,6 +1,33 @@
-import Image from 'next/image'
+'use client'
+import { useEffect, useState } from 'react'
+import { pieces } from './helper'
+import {
+  Hex,
+  HexGrid,
+  Hexagon,
+  Layout,
+  Path,
+  Pattern,
+  GridGenerator,
+} from 'react-hexgrid'
 
 const Gameroom = () => {
+  const [boardState, setBoardState] = useState(pieces)
+  const hexagons = GridGenerator.hexagon(4).map((hex, index) => ({
+    ...hex,
+    content: `${hex.q},${hex.r}`, // Using q and r coordinates for content
+    key: `hex-${index}`, // Adding a unique key
+  }))
+
+  const hexWidth = 5
+
+  useEffect(() => {
+    document.querySelectorAll('.hexagon-group').forEach((element) => {
+      element.addEventListener('click', () => {
+        console.log('clicked class')
+      })
+    })
+  }, [])
   return (
     <div className="flex align-middle justify-around h-[85vh]">
       <div className="hidden md:flex md:items-center">settings</div>
@@ -9,7 +36,39 @@ const Gameroom = () => {
           <span className="text-white text-[1.5em]">Player 1</span>
           <span className="text-white text-[1.5em]">04:12</span>
         </div>
-        <Image width={550} height={550} alt="no board" src="/boardRoom.png" />
+        <HexGrid
+          width={document.documentElement.clientWidth < 500 ? 350 : 600}
+          height={800}
+        >
+          <Layout
+            size={{ x: 3.8, y: 3.8 }}
+            flat={true}
+            origin={{ x: 0, y: 0 }}
+            spacing={1.9}
+          >
+            {hexagons.map((hex) => (
+              <Hexagon
+                key={hex.key}
+                onClick={() => console.log(Number(hex.key.slice(4)))}
+                q={hex.q}
+                spacing={1}
+                r={hex.r}
+                fill="#f3f3f3" // Set hexagon fill color
+                color="red"
+                stroke={
+                  Number(hex.key.slice(4)) % 2 === 0
+                    ? '#f33333'
+                    : Number(hex.key.slice(4)) % 3 === 0
+                    ? '#000000'
+                    : '#f0f0f0f0'
+                }
+                strokeWidth={6}
+                s={hex.s}
+                alignmentBaseline="hanging"
+              ></Hexagon>
+            ))}
+          </Layout>
+        </HexGrid>
         <div className="flex justify-between">
           <span className="text-white text-[1.5em]">04:33</span>
           <span className="text-white text-[1.5em]">Player 2</span>
